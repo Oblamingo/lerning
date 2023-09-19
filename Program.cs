f7e4ea679ceb0674ebae2afa6e86a13e3c26f298
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2023-09-19 10:30:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+bool flagCorrect = true;
+int countCoins = 0;
+uint totalCountCoins = 0;
+long totalTimeTicks = 0;
+byte byteString = 0;
+sbyte byteSString = -1;
+float shortDouble = 0;
+double myDouble = 0;
+char mainChar = '$';
+string userName = "The One";
+
+Console.WriteLine("Hello, Teacher!");
