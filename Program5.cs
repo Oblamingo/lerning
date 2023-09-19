93f897f33142d9c06ef246962c3783563929e5ff
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2023-09-19 14:38:11 +0300

    Update Program5.cs

--- Program5.cs
+++ Program5.cs
@@ -1,11 +1,12 @@
 Console.WriteLine($"Сколько у Вас монет?");
 int.TryParse(Console.ReadLine(), out int yourCouins);
 
-int cristPrice = 3;
+int crystalPrice = 3;
 
-Console.WriteLine($"Сколько купить кристаллов?\r\nЦена кристала {cristPrice}");
+Console.WriteLine($"Цена кристала {crystalPrice}\r\nСколько купить кристаллов?");
 int.TryParse(Console.ReadLine(), out int countItems);
 
-yourCouins -= countItems*cristPrice;
+yourCouins -= countItems * crystalPrice;
 
 Console.WriteLine($"В инвентаре {countItems} кристаллов {yourCouins} монет");
+
