5c8d8e568234b10753e5afbfeaa1b23678988a8b
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2023-09-19 13:44:15 +0300

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -1,9 +1,9 @@
 Console.WriteLine("Здраствуйте!\r\nВведите Ваше имя пожалуйста:");
-var name = Console.ReadLine();
+string name = Console.ReadLine();
 Console.WriteLine("Подскажите, сколько Вам полных лет:");
-var old = Console.ReadLine();
+string old = Console.ReadLine();
 Console.WriteLine("Пожалуйста, укажите знак зодиака:");
-var typeMonth = Console.ReadLine();
+string typeMonth = Console.ReadLine();
 Console.WriteLine("Расскажите, где вы работаете:");
-var workPlace = Console.ReadLine();
+string workPlace = Console.ReadLine();
 Console.WriteLine($"Вас зовут {name}, Вам {old} год, Вы {typeMonth} и работаете {workPlace}.");
