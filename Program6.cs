6c4ab7059c419ab8f7b02425cb2d27c24b147d42
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2023-09-25 21:18:16 +0300

    Update Program6.cs

--- Program6.cs
+++ Program6.cs
@@ -1,11 +1,11 @@
 Console.WriteLine("Введите кол-во людей в очереди:");
-int.TryParse(Console.ReadLine(), out int countPeople);
+int.TryParse(Console.ReadLine(), out int peopleCount);
 
 int timeForOnePerson = 10;
 int timeDivider = 60;
 
-int fullTimeMinutes = countPeople * timeForOnePerson;
+int fullTimeMinutes = peopleCount * timeForOnePerson;
 int hoursWait = fullTimeMinutes / timeDivider;
 int minutesWait = fullTimeMinutes % timeDivider;
 
-Console.WriteLine($"В очереди {countPeople} человек, ваше время ожидания {hoursWait}ч. {minutesWait}мин.");
+Console.WriteLine($"В очереди {peopleCount} человек, ваше время ожидания {hoursWait}ч. {minutesWait}мин.");
