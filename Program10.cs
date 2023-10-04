156f131639a5f5e60f3bc9aad49d091a45a928cf
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2023-10-04 19:15:19 +0300

    Update Program10.cs

--- Program10.cs
+++ Program10.cs
@@ -1,32 +1,22 @@
 Random randomizer = new Random();
 
 int randomNumber = 0;
-int sumNumbers3 = 0;
-int sumNumbers5 = 0;
-int sumNumbersAll = 0;
+int sumNumbers = 0;
 int firstMagicDivider = 3;
 int secondMagicDivider = 5;
-int startPeriodNumber = 100;
-string magicalNumberString3 = "";
-string magicalNumberString5 = "";
+int endRandomizerNumber = 100;
+
 ConsoleColor defaultColor = Console.ForegroundColor;
 
-randomNumber = randomizer.Next(firstMagicDivider, startPeriodNumber);
+randomNumber = randomizer.Next(firstMagicDivider, endRandomizerNumber);
 
-for (int i = 0; i <= randomNumber; i++)
+for (int i = firstMagicDivider; i <= randomNumber; i++)
 {
-    if (i % firstMagicDivider == 0 && i / firstMagicDivider != 0)
+    if (i % firstMagicDivider == 0 || i % secondMagicDivider == 0)
     {
-        sumNumbers3 += i;
-        magicalNumberString3 += $"{i} ";
-    }
-    else if (i % secondMagicDivider == 0 && i / secondMagicDivider != 0)
-    {
-        sumNumbers5 += i;
-        magicalNumberString5 += $"{i} ";
+        sumNumbers += i;
     }
 }
-sumNumbersAll = sumNumbers3 + sumNumbers5;
 
 Console.Write($"Случайное число : ");
 
@@ -34,21 +24,9 @@ Console.ForegroundColor = ConsoleColor.Green;
 Console.Write($"{randomNumber}");
 
 Console.ForegroundColor = defaultColor;
-Console.Write($"\r\nЧисла кратные {firstMagicDivider} и их сумма: ");
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.Write($"{magicalNumberString3}({sumNumbers3})");
-
-Console.ForegroundColor = defaultColor;
-Console.Write($"\r\nЧисла кратные {secondMagicDivider} и их сумма: ");
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.Write($"{magicalNumberString5}({sumNumbers5})");
-
-Console.ForegroundColor = defaultColor;
 Console.Write($"\r\nСумма кратных {firstMagicDivider} и {secondMagicDivider} числам: ");
 
 Console.ForegroundColor = ConsoleColor.Green;
-Console.Write($"{sumNumbersAll}");
+Console.Write($"{sumNumbers}");
 
 Console.ForegroundColor = defaultColor;
