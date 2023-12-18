4076b73806e157c99920c610505b5daaf651b8bc
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2023-12-18 09:23:26 +0300

    Update Program12.cs

--- Program12.cs
+++ Program12.cs
@@ -1,17 +1,19 @@
 Console.Title = "USA Department";
 
-const int MenuChangeName = 1;
-const int MenuChangePassword = 2;
-const int MenuChangeColorText = 3;
-const int MenuGetCage = 4;
-const int MenuExit = 9;
-const int MenuColorRed = 1;
-const int MenuColorGreen = 2;
+const string CommandChangeName = "1";
+const string CommandChangePassword = "2";
+const string MenuChangeColorText = "3";
+const string MenuGetCage = "4";
+const string MenuExit = "9";
+const string MenuColorRed = "1";
+const string MenuColorGreen = "2";
 
-int menuNavigatorType = 0;
+string menuNavigatorType = "0";
 int silienceNumber = 0;
 int countTries = 3;
 int userGameNumber = 0;
+int minimumRandomNumber = 1;
+int maximumRandomNumber = 5;
 
 bool isWorking = true;
 bool isPasswordCorrect = false;
@@ -24,10 +26,15 @@ ConsoleColor consoleColorRed = ConsoleColor.Red;
 ConsoleColor consoleColorName = ConsoleColor.Magenta;
 ConsoleColor userConsoleColor = consoleColorDeafualt;
 
-Random randomNumers = new Random();
+ConsoleKey isUserWantPlay = ConsoleKey.Y;
 
-string userName = "admin";
-string userPassword = "admin";
+ConsoleKeyInfo userInputKey = new ConsoleKeyInfo();
+
+Random random = new Random();
+
+string defaultName = "admin";
+string userName = defaultName;
+string userPassword = defaultName;
 string authorisationName = string.Empty;
 string authorisationPassword = string.Empty;
 
@@ -46,132 +53,146 @@ while (isWorking)
         Console.ForegroundColor = consoleColorName;
         Console.WriteLine($"Добрый день агент, {userName}!");
         Console.ForegroundColor = consoleColorDeafualt;
-        Console.WriteLine(  $"[{MenuChangeName}] Изменить имя\r\n" +
-                            $"[{MenuChangePassword}] Изменить пароль\r\n" +
+        Console.WriteLine($"[{CommandChangeName}] Изменить имя\r\n" +
+                            $"[{CommandChangePassword}] Изменить пароль\r\n" +
                             $"[{MenuChangeColorText}] Изменить цвет ввода текста\r\n" +
                             $"[{MenuGetCage}] Угадать код от сейфа\r\n" +
                             $"[{MenuExit}] Выход из программы");
+
         Console.ForegroundColor = userConsoleColor;
+        menuNavigatorType = Console.ReadLine() ?? MenuExit;
 
-        if (int.TryParse(Console.ReadLine(), out menuNavigatorType))
+        switch (menuNavigatorType)
         {
-            switch (menuNavigatorType)
-            {
-                case MenuChangeName:
-                    Console.ForegroundColor = consoleColorDeafualt;
-                    Console.Write(messageChangeName);
+            case CommandChangeName:
+                Console.ForegroundColor = consoleColorDeafualt;
+                Console.Write(messageChangeName);
 
-                    Console.ForegroundColor = userConsoleColor;
-                    userName = Console.ReadLine();
+                Console.ForegroundColor = userConsoleColor;
+                userName = Console.ReadLine();
 
-                    isPasswordCorrect = false;
-                    break;
+                if (string.IsNullOrWhiteSpace(userName))
+                {
+                    userName = defaultName;
 
-                case MenuChangePassword:
-                    Console.ForegroundColor = consoleColorDeafualt;
-                    Console.Write(messageChangePassword);
+                    Console.ForegroundColor = consoleColorRed;
+                    Console.WriteLine(errorInputWrong);
+                    Console.ReadKey();
+                }
 
-                    Console.ForegroundColor = userConsoleColor;
-                    userPassword = Console.ReadLine();
+                isPasswordCorrect = false;
+                break;
 
-                    isPasswordCorrect = false;
-                    break;
+            case CommandChangePassword:
+                Console.ForegroundColor = consoleColorDeafualt;
+                Console.Write(messageChangePassword);
 
-                case MenuChangeColorText:
-                    Console.ForegroundColor = consoleColorDeafualt;
-                    Console.WriteLine($"Выберите цвет ввода:" +
-                                      $"[{MenuColorRed}] Красный\r\n" +
-                                      $"[{MenuColorGreen}] Зелёный\r\n");
+                Console.ForegroundColor = userConsoleColor;
+                userPassword = Console.ReadLine();
 
-                    if (int.TryParse(Console.ReadLine(), out menuNavigatorType))
+                if (string.IsNullOrWhiteSpace(userName))
+                {
+                    userName = defaultName;
+
+                    Console.ForegroundColor = consoleColorRed;
+                    Console.WriteLine(errorInputWrong);
+                    Console.ReadKey();
+                }
+
+                isPasswordCorrect = false;
+                break;
+
+            case MenuChangeColorText:
+                Console.ForegroundColor = consoleColorDeafualt;
+                Console.WriteLine($"Выберите цвет ввода:" +
+                                  $"[{MenuColorRed}] Красный\r\n" +
+                                  $"[{MenuColorGreen}] Зелёный\r\n");
+
+                Console.ForegroundColor = userConsoleColor;
+                menuNavigatorType = Console.ReadLine();
+
+                switch (menuNavigatorType)
+                {
+                    case MenuColorRed:
+                        userConsoleColor = consoleColorRed;
+                        break;
+
+                    case MenuColorGreen:
+                        userConsoleColor = consoleColorGreen;
+                        break;
+                    default:
+                        userConsoleColor = consoleColorDeafualt;
+                        break;
+                }
+                break;
+
+            case MenuGetCage:
+                while (isPlaying)
+                {
+                    Console.Clear();
+                    silienceNumber = random.Next(minimumRandomNumber, maximumRandomNumber);
+                    isWinInGame = false;
+                    countTries = 5;
+
+                    while (!isWinInGame && countTries > 0)
                     {
-                        switch (menuNavigatorType)
+                        Console.ForegroundColor = consoleColorDeafualt;
+                        Console.WriteLine($"Введите предполагаемое число. У вас {countTries} попыток");
+
+                        Console.ForegroundColor = userConsoleColor;
+
+                        if (int.TryParse(Console.ReadLine(), out userGameNumber))
                         {
-                            case MenuColorRed:
-                                userConsoleColor = consoleColorRed;
-                                break;
-
-                            case MenuColorGreen:
-                                userConsoleColor = consoleColorGreen;
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = consoleColorRed;
-                        Console.WriteLine(errorInputWrong);
-                    }
-                    break;
-
-                case MenuGetCage:
-                    while (isPlaying)
-                    {
-                        Console.Clear();
-                        silienceNumber = randomNumers.Next(1, 2);
-                        isWinInGame = false;
-                        countTries = 5;
-
-                        while (!isWinInGame && countTries > 0)
-                        {
-                            Console.ForegroundColor = consoleColorDeafualt;
-                            Console.WriteLine($"Введите предполагаемое число. У вас {countTries} попыток");
-
-                            Console.ForegroundColor = userConsoleColor;
-
-                            if (int.TryParse(Console.ReadLine(), out userGameNumber))
+                            if (userGameNumber == silienceNumber)
                             {
-                                if (userGameNumber == silienceNumber)
-                                {
-                                    isWinInGame = true;
-                                    continue;
-                                }
-                                else
-                                {
-                                    countTries--;
-
-                                    Console.ForegroundColor = consoleColorRed;
-                                    Console.WriteLine(errorWrongTryGame);
-                                }
+                                isWinInGame = true;
                             }
                             else
                             {
                                 countTries--;
 
                                 Console.ForegroundColor = consoleColorRed;
-                                Console.WriteLine(errorInputWrong);
+                                Console.WriteLine(errorWrongTryGame);
                             }
-                        }
-
-                        if (isWinInGame)
-                        {
-                            Console.ForegroundColor = consoleColorGreen;
-                            Console.WriteLine("Ты угадал код!");
                         }
                         else
                         {
-                            Console.ForegroundColor = consoleColorGreen;
-                            Console.WriteLine("Ты проиграл все попытки!");
-                        }
+                            countTries--;
 
-                        Console.ForegroundColor = consoleColorDeafualt;
-                        Console.WriteLine("Сыграем снова?Y/N\r\n");
-
-                        Console.ForegroundColor = userConsoleColor;
-                        ConsoleKeyInfo key = Console.ReadKey();
-
-                        if (key.Key != ConsoleKey.Y)
-                        {
-                            isPlaying = false;
-                            continue;
+                            Console.ForegroundColor = consoleColorRed;
+                            Console.WriteLine(errorInputWrong);
                         }
                     }
-                    break;
 
-                case MenuExit:
-                    isWorking = false;
-                    break;
-            }
+                    if (isWinInGame)
+                    {
+                        Console.ForegroundColor = consoleColorGreen;
+                        Console.WriteLine("Ты угадал код!");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = consoleColorGreen;
+                        Console.WriteLine("Ты проиграл все попытки!");
+                    }
+
+                    Console.ForegroundColor = consoleColorDeafualt;
+                    Console.WriteLine("Сыграем снова?Y/N\r\n");
+
+                    Console.ForegroundColor = userConsoleColor;
+                    userInputKey = Console.ReadKey();
+
+                    if (userInputKey.Key != isUserWantPlay)
+                    {
+                        isPlaying = false;
+                    }
+                }
+                break;
+
+            case MenuExit:
+                isWorking = false;
+                break;
         }
+
     }
     else
     {
