f878c6f6e9d98c556b0c53b428829bba605d94fe
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2024-03-04 19:45:34 +0300

    Update Program20.cs

--- Program20.cs
+++ Program20.cs
@@ -1,6 +1,6 @@
 Random random = new Random();
-int randMin = 1;
-int randMax = 11;
+int randomMin = 1;
+int randomMax = 11;
 int rowNumber = 1;
 int columnNumber = 0;
 int sumLine = 0;
@@ -13,20 +13,21 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = random.Next(randMin, randMax);
+        array[i, j] = random.Next(randomMin, randomMax);
         Console.Write($"{array[i, j]} ");
-
-        if (i == rowNumber)
-        {
-            sumLine += array[i, j];
-        }
-
-        if (j == columnNumber)
-        {
-            multiplyColumn *= array[i, j];
-        }
     }
+
     Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    sumLine += array[rowNumber, i];
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    multiplyColumn *= array[i, columnNumber];
 }
 
 Console.WriteLine($"\nСумма чисел {rowNumber+1} ряда: {sumLine}"+
