66cbdbf0b23f87f87d3010e729fed989830c9ab5
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2024-03-06 17:54:17 +0000

    Update Program23.cs

--- Program23.cs
+++ Program23.cs
@@ -1,76 +1,81 @@
-const string MenuSum = "sum";
-const string MenuExit = "exit";
+using System;
 
-Random random = new Random();
-
-bool isEnter = true;
-
-int sumNumbers = 0;
-int arraySize = 0;
-int inputInt = 0;
-
-string inputLine = string.Empty;
-string messageTemp = string.Empty;
-
-
-int[] arrayNumbers = new int[arraySize];
-int[] arrayTemp = new int[arraySize];
-
-while (isEnter)
+namespace Lerning
 {
-    if (arrayNumbers.Length > 0)
+    internal class Program
     {
-        foreach (int i in arrayNumbers)
+        static void Main(string[] args)
         {
-            messageTemp += $"{i} ";
-        }
+            const string MenuSum = "sum";
+            const string MenuExit = "exit";
 
-        Console.WriteLine($"Entered array:{messageTemp}");
+            bool isEnter = true;
 
-        messageTemp = string.Empty;
-    }
+            int sumNumbers = 0;
+            int arraySize = 0;
+            int inputInt = 0;
 
-    Console.WriteLine("Enter command:");
+            string inputLine = string.Empty;
+            string messageTemp = string.Empty;
 
-    inputLine = Console.ReadLine().ToLower();
+            int[] arrayNumbers = new int[arraySize];
+            int[] arrayTemp = new int[arraySize];
 
-    switch (inputLine)
-    {
-        case MenuSum:
-
-            foreach (int i in arrayNumbers)
-                sumNumbers += i;
-
-            Console.WriteLine($"Sum = {sumNumbers}\nPress any key. . .");
-            Console.ReadKey();
-            break;
-
-        case MenuExit:
-
-            isEnter = false;
-            break;
-
-        default:
-
-            if (int.TryParse(inputLine, out inputInt))
+            while (isEnter)
             {
-                arrayTemp = new int[arrayNumbers.Length + 1];
-
-                for (int i = 0; i < arrayNumbers.Length; i++)
+                if (arrayNumbers.Length > 0)
                 {
-                    arrayTemp[i] = arrayNumbers[i];
+                    foreach (int i in arrayNumbers)
+                    {
+                        messageTemp += $"{i} ";
+                    }
+
+                    Console.WriteLine($"Entered array:{messageTemp}");
+
+                    messageTemp = string.Empty;
                 }
 
-                arrayTemp[arrayTemp.Length - 1] = inputInt;
-                arrayNumbers = arrayTemp;
-            }
-            else
-            {
-                Console.WriteLine("Wrong command\nPress any key. . .");
-                Console.ReadKey();
-            }
-            break;
-    }
+                Console.WriteLine("Enter command:");                
 
-    Console.Clear();
+                inputLine = Console.ReadLine().ToLower();
+
+                switch (inputLine)
+                {
+                    case MenuSum:
+                        foreach (int i in arrayNumbers)
+                            sumNumbers += i;
+
+                        Console.WriteLine($"Sum = {sumNumbers}\nPress any key. . .");
+                        Console.ReadKey();
+                        break;
+
+                    case MenuExit:
+                        isEnter = false;
+                        break;
+
+                    default:
+                        if (int.TryParse(inputLine, out inputInt))
+                        {
+                            arrayTemp = new int[arrayNumbers.Length + 1];
+
+                            for (int i = 0; i < arrayNumbers.Length; i++)
+                            {
+                                arrayTemp[i] = arrayNumbers[i];
+                            }
+
+                            arrayTemp[arrayTemp.Length - 1] = inputInt;
+                            arrayNumbers = arrayTemp;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong command\nPress any key. . .");
+                            Console.ReadKey();
+                        }
+                        break;
+                }
+
+                Console.Clear();
+            }
+        }
+    }
 }
