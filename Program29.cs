1d36b2ac552250dae3ac99f5ec2c87d2b2ad74b5
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2024-03-09 16:24:27 +0300

    Update Program29.cs

--- Program29.cs
+++ Program29.cs
@@ -12,37 +12,27 @@ namespace Lerning
             int persentHealth = 0;
 
             char symbolHealth = '#';
-            char symbolEmpty = '_';
+            char symbolEmpty = ' ';
 
             persentHealth = (health * persentFull) / maxHealth;
 
             DrawBar(persentHealth, symbolHealth, symbolEmpty);
         }
 
-        static void DrawBar(int persentHealth, char symbolHealth,char symbolEmpty)
+        static void DrawBar(int persentHealth, char symbolHealth, char symbolEmpty)
         {
             string healthPrefix = "Health ";
-            string lineOfSymbols = string.Empty;
 
             int healthInBar = 0;
             int lengthBar = 10;
-            
-            healthInBar = persentHealth / lengthBar;            
+
+            healthInBar = persentHealth / lengthBar;
 
             if (healthInBar >= 0 && healthInBar <= lengthBar)
             {
-                for (int i = 0; i < healthInBar; i++)
-                {
-                    lineOfSymbols += symbolHealth;
-                }               
-
-                for (int i = healthInBar; i < lengthBar; i++)
-                {
-                    lineOfSymbols += symbolEmpty;
-                }
-
                 Console.Write($"{healthPrefix}{persentHealth}%\n" +
-                              $"{healthPrefix}[{lineOfSymbols}]");
+                              $"{healthPrefix}[{GenerateLineSymbols(symbolHealth, healthInBar)}" +
+                                             $"{GenerateLineSymbols(symbolEmpty, lengthBar - healthInBar)}]");
             }
             else
             {
@@ -50,6 +40,18 @@ namespace Lerning
             }
 
             Console.ReadKey();
+        }
+
+        static string GenerateLineSymbols(char symbolHealth, int lengthOfSybols)
+        {
+            string line = string.Empty;
+
+            for (int i = 0; i < lengthOfSybols; i++)
+            {
+                line += symbolHealth;
+            }
+
+            return line;
         }
     }
 }
