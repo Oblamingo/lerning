c1ff8b46c2507ce57ce462ab9fc4ce7c56fca6cd
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2024-03-09 19:06:25 +0300

    Create Program30.cs

---
+++ Program30.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Lerning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            ShowArray(array);
+            Shuffle(array);
+            ShowArray(array);
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            int arrayIndex;
+            int tempElement;
+
+            for (int i =0 ; i <= array.Length - 1;  i++)
+            {
+                arrayIndex = random.Next(i, array.Length);
+                tempElement = array[arrayIndex];
+                array[arrayIndex] = array[i];
+                array[i] = tempElement;                
+            }
+        }
+        static void ShowArray(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
