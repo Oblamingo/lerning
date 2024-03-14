5783e0e38566f707fe05a817dd9100af256c5df7
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2024-03-14 19:51:23 +0300

    Create Program37.cs

---
+++ Program37.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lerning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            int[] firstArray = { 1, 2, 1 };
+            int[] secondArray = { 3, 2 };
+
+            AddValueToList(numbers, firstArray);
+            AddValueToList(numbers, secondArray);
+            DrawList(numbers);
+        }
+
+        private static void AddValueToList(List<int> numbers, int[] array)
+        {
+            bool notIncluded = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (numbers.Contains(array[i]) == notIncluded)
+                {
+                    numbers.Add(array[i]);
+                }
+            }
+        }
+
+        private static void DrawList(List<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
