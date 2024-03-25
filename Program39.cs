e66073674650f75fdc1c4bbabdfb80e3ba784aba
Oblamingo <144927789+Oblamingo@users.noreply.github.com>
2024-03-25 22:49:20 +0300

    Create Program39.cs

---
+++ Program39.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Lerning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int positionX = 10;
+            int positionY = 10;
+
+            char characterSymbol = '@';
+            
+            Player player = new Player(positionX, positionY, characterSymbol);
+            Render render = new Render();
+
+            render.DrawCharacter(player);
+
+            Console.ReadKey();
+        }      
+    }
+
+    class Player
+    {
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+        public char Symbol { get; private set; }
+
+        public Player(int positionX, int positionY, char symbol)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+            Symbol = symbol;
+        }
+    }
+
+    class Render
+    {
+        public void DrawCharacter(Player character)
+        {
+            Console.SetCursorPosition(character.PositionX, character.PositionY);
+            Console.WriteLine(character.Symbol);
+        }
+    }
+}
